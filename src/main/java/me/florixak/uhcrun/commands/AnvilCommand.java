5fc44c6dfdb9f0dd42f5223e7d647de9ff3035b5
florixak <florixakcz@seznam.cz>
2024-06-03 11:35:54 +0200

    Update AnvilCommand.java

--- src/main/java/me/florixak/uhcrun/commands/AnvilCommand.java
+++ src/main/java/me/florixak/uhcrun/commands/AnvilCommand.java
@@ -4,12 +4,12 @@ import me.florixak.uhcrun.UHCRun;
 import me.florixak.uhcrun.config.Messages;
 import me.florixak.uhcrun.game.GameManager;
 import net.wesjd.anvilgui.AnvilGUI;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-
-import java.util.Arrays;
+import org.bukkit.inventory.ItemStack;
 
 import static me.florixak.uhcrun.game.Permissions.ANVIL;
 
@@ -32,18 +32,29 @@ public class AnvilCommand implements CommandExecutor {
             return true;
         }
 
+        /*Location location = p.getLocation();
+        World world = p.getWorld();
+        Block block = world.getBlockAt(location);
+        block.setType(Material.ANVIL);
+        p.openInventory(block.getState());
+        block.setType(Material.AIR);*/
+
+
         new AnvilGUI.Builder()
-                .onClose((slot) -> {
-                    p.getInventory().addItem(slot.getLeftItem());
+                .onClose(playerClose -> { //Called when the inventory is closing
+                    System.out.println("The inventory is closing");
                 })
-                .onClick((slot, stateSnapshot) -> {
-                    if (slot == AnvilGUI.Slot.OUTPUT && stateSnapshot.getOutputItem() != null) {
-                        p.getInventory().addItem(stateSnapshot.getOutputItem());
-                    }
-                    return Arrays.asList(AnvilGUI.ResponseAction.close());
+                .onClick((playerComplete, text) -> { //Called when the text is submitted
+                    System.out.println("The player has submitted the text: " + text);
+                    return AnvilGUI.Response.close();
                 })
-                .plugin(UHCRun.getInstance())
-                .open(p);
+                .text("") //Sets the default text that will be in the text box
+                .itemLeft(new ItemStack(Material.AIR)) //Sets the item on the left of the GUI
+                .itemRight(new ItemStack(Material.AIR)) //Sets the item on the right of the GUI
+                .title("Example GUI") //Sets the title of the GUI (only works in 1.14 and above, otherwise use a plugin)
+                .plugin(UHCRun.getInstance()) //Set the plugin instance
+                .open(p); //Opens the GUI for the player provided
+
         return true;
     }
 }
