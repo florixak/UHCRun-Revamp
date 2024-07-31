f6974235f59a62e67338410a7a8aa20174ad0231
florixak <florixakcz@seznam.cz>
2024-07-31 22:13:44 +0200

    Update WorldGeneratorListener.java

--- src/main/java/me/florixak/uhcrevamp/game/worldGenerator/WorldGeneratorListener.java
+++ src/main/java/me/florixak/uhcrevamp/game/worldGenerator/WorldGeneratorListener.java
@@ -2,6 +2,7 @@ package me.florixak.uhcrevamp.game.worldGenerator;
 
 import me.florixak.uhcrevamp.game.GameValues;
 import me.florixak.uhcrevamp.utils.XSeries.XBiome;
+import org.bukkit.Bukkit;
 import org.bukkit.Chunk;
 import org.bukkit.World;
 import org.bukkit.block.Biome;
@@ -18,21 +19,25 @@ public class WorldGeneratorListener implements Listener {
 	public void onWorldLoad(WorldLoadEvent event) {
 		World world = event.getWorld();
 		if (!world.getName().equalsIgnoreCase(GameValues.WORLD_NAME)) return;
-
-		for (Chunk chunk : world.getLoadedChunks()) {
-			for (int x = 0; x < 16; x++) {
-				for (int z = 0; z < 16; z++) {
-					XBiome xBiome = XBiome.matchXBiome(chunk.getBlock(x, world.getHighestBlockYAt(x, z), z).getBiome());
-					if (xBiome.isSupported()) {
-						Biome biome = xBiome.getBiome();
-						if (GameValues.GAME.DISABLED_BIOMES.contains(biome.name())) {
-							for (int y = 0; y < world.getMaxHeight(); y++) {
-								chunk.getBlock(x, y, z).setBiome(Biome.PLAINS);
+		try {
+			for (Chunk chunk : world.getLoadedChunks()) {
+				for (int x = 0; x < 16; x++) {
+					for (int z = 0; z < 16; z++) {
+						XBiome xBiome = XBiome.matchXBiome(chunk.getBlock(x, world.getHighestBlockYAt(x, z), z).getBiome());
+						if (xBiome.isSupported()) {
+							Biome biome = xBiome.getBiome();
+							if (GameValues.GAME.DISABLED_BIOMES.contains(biome.name())) {
+								for (int y = 0; y < world.getMaxHeight(); y++) {
+									chunk.getBlock(x, y, z).setBiome(Biome.PLAINS);
+								}
 							}
 						}
 					}
 				}
 			}
+		} catch (Exception ignore) {
+			Bukkit.getLogger().info("Failed to replace biomes.");
 		}
+
 	}
 }
